fbbce66e942877fb69c4f4ee045aa22e20010464
tungminh111 <dmlocdmloc@gmail.com>
2021-09-13 22:26:30 +0700

    push bug len master

--- components/edit_article/EditArticle.js
+++ components/edit_article/EditArticle.js
@@ -1,10 +1,27 @@
-import { VStack } from "@chakra-ui/react";
+import {
+  Input,
+  Textarea,
+  InputGroup,
+  VStack,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+require("medium-editor/dist/css/medium-editor.css");
+require("medium-editor/dist/css/themes/default.css");
+
+import Editor from "react-medium-editor";
 import EditArticleHeader from "./EditArticleHeader";
 
 function EditArticle(props) {
+  const [title, setTitle] = useState("");
+
   return (
     <VStack>
       <EditArticleHeader w="70vw" data={props.userData} />
+      <VStack w="70vw">
+        <Editor text={title} onChange={(text, medium) => setTitle(text)} />
+      </VStack>
     </VStack>
   );
 }
