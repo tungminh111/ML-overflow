791c5ae4d27f13a1deafa58f1a0eb048fccb5651
longvb <vuongbaolong48@gmail.com>
2021-09-14 23:12:21 +0700

    fix bac

--- components/edit_article/EditArticle.js
+++ components/edit_article/EditArticle.js
@@ -1,31 +1,35 @@
 import {
-  Input,
-  Textarea,
-  InputGroup,
-  Flex,
-  VStack,
-  InputLeftAddon,
+    Input,
+    Textarea,
+    InputGroup,
+    Flex,
+    VStack,
+    InputLeftAddon,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
-import MediumEditor from "medium-editor";
 import EditArticleHeader from "./EditArticleHeader";
+import "medium-editor/dist/css/medium-editor.css";
 
-class EditArticle extends React.Component {
-  componentDidMount() {
-    const editor = new MediumEditor(".medium-editable", {});
-  }
-
-  render() {
+function EditArticle(props) {
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            import("medium-editor").then((modulde) => {
+                const MediumEditor = modulde.default;
+                const editor = new MediumEditor(".medium-editable", {
+                    placeholder: "cai cc",
+                });
+            });
+        }
+    }, []);
     return (
-      <Flex flexDir="column" w="70vw" ms="15vw">
-        <EditArticleHeader data={this.props.userData} />
-        <div>
-          <textarea id="medium-editable" className="medium-editable" />
-        </div>
-      </Flex>
+        <Flex flexDir="column" w="70vw" ms="15vw">
+            <EditArticleHeader data={props.userData} />
+            <div>
+                <textarea id="medium-editable" className="medium-editable" />
+            </div>
+        </Flex>
     );
-  }
 }
 
 export default EditArticle;
